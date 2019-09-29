8af65c2b3a3f03fb0b1f07f57d6e54c87251dd20
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2019-09-29 11:37:46 +0300

    converted to Swift Package, Swift 5

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import QuickTableViewControllerTests
+
+var tests = [XCTestCaseEntry]()
+tests += QuickTableViewControllerTests.allTests()
+XCTMain(tests)
--- scripts/swiftlint.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/local/env ruby
-
-not_ci = !ENV["TRAVIS"] || ENV["TRAVIS"]&.empty?
-
-if not_ci
-  system "${PODS_ROOT}/SwiftLint/swiftlint --config .swiftlint.yml"
-else
-  puts "Skip SwiftLint"
-end
